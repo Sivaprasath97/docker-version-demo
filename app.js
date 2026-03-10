dbde86cc81b13e76b824690cf31b91e592a6511c
Sivaprasath S S <sivaprasath061@gmail.com>
2026-03-10 09:59:04 +0530

    Version 2 update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello from Version 1");
+    res.send("Hello from Version 2 🚀");
 });
 
 app.listen(3000,()=>{
